66ac3576cc388269f5d8ab5d5c7bf32f465a4e92
Matthias Reuter <matthias.reuter@inovex.de>
2014-01-21 11:17:01 +0100

    Save Math object in a variable. Down to 144 chars.

--- getRandomString.js
+++ getRandomString.js
@@ -1,3 +1,3 @@
 function getRandomString(len) {
-var t="",p="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",i=len;while(i--)t+=p.charAt(Math.floor(Math.random()*p.length));return t;
+var M=Math,t="",p="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",i=len;while(i--)t+=p.charAt(M.floor(M.random()*62));return t;
 }
