5df406b7268a84027bb37a9bd87595b434332f1e
Matthias Reuter <matthias.reuter@inovex.de>
2014-01-21 11:22:33 +0100

    Removed whitespace. Down to 99 chars.

--- getRandomString.js
+++ getRandomString.js
@@ -1,9 +1,3 @@
 function getRandomString(len) {
-    var t = "", r;
-    while (len--) {
-        r = Math.random() * 62;
-        r+= r > 35 ? 61 : r > 9 ? 55 : 48;
-        t += String.fromCharCode(r)
-    }
-    return t;
+var t="",r;while(len--){r=Math.random()*62;r+=r>35?61:r>9?55:48;t+=String.fromCharCode(r)}return t;
 }
