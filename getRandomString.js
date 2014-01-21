d5a1693c0903bd9e23e3f43e26960f18d5acf064
Matthias Reuter <matthias.reuter@inovex.de>
2014-01-21 11:19:34 +0100

    Improved loop initialisation. Down to 124 chars.

--- getRandomString.js
+++ getRandomString.js
@@ -1,3 +1,3 @@
 function getRandomString(len) {
-var t="",p="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",i=len;while(i--)t+=p[~~(Math.random()*62)];return t;
+var t="",p="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";while(len--)t+=p[~~(Math.random()*62)];return t;
 }
