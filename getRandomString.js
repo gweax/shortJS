2df8a12381fa8de5dd404835542ac5af5f4e0c68
Matthias Reuter <matthias.reuter@inovex.de>
2014-01-21 11:16:23 +0100

    Iterate backwards in while loop. Down to 149 chars.

--- getRandomString.js
+++ getRandomString.js
@@ -1,3 +1,3 @@
 function getRandomString(len) {
-var t="",p="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",i=0;while(i<len){t+=p.charAt(Math.floor(Math.random()*p.length));i++;}return t;
+var t="",p="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",i=len;while(i--)t+=p.charAt(Math.floor(Math.random()*p.length));return t;
 }
