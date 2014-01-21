490f78117ea13e2a69949a78ee2eb10a6223c09a
Matthias Reuter <matthias.reuter@inovex.de>
2014-01-21 11:57:05 +0100

    Down to 101 chars.

--- getRandomString.js
+++ getRandomString.js
@@ -1,3 +1,3 @@
 function getRandomString(len) {
-var t="",r;while(len--){r=~~(Math.random()*62);r+=r>35?61:r>9?55:48;t+=String.fromCharCode(r)}return t;
+var t="",r;while(len--)t+=String.fromCharCode((r=~~(Math.random()*62))+(r>35?61:r>9?55:48));return t;
 }
